4a3f7e9a084406994a6f408c448ab789c2aa37e1
xytoki <7547189+xytoki@users.noreply.github.com>
2020-03-03 07:41:14 +0800

    修复中文路径rules问题

--- _app/lib/TC.php
+++ _app/lib/TC.php
@@ -68,5 +68,9 @@ Class TC{
         $cache->save($item);
         return $key;
     }
-
+    static function encodeURI(String $uri){
+        return preg_replace_callback("{[^0-9a-z_.!~*'();,/?:@&=+$#-]}i", function ($m) {
+            return sprintf('%%%02X', ord($m[0]));
+        }, $uri);
+    }
 }
